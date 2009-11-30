8d3023fa79e7fbdca72d7c63fb768771a554931d
pattersonc <pattersonc@comcast.net>
2009-11-29 23:38:55 -0500

    area stuff

---
+++ MyPhotos.WebUI/Areas/AlbumArea/AreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MyPhotos.WebUI.Areas.AlbumArea
+{
+    public class AlbumAreaAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "AlbumArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "AlbumArea_default",
+                "AlbumArea/{controller}/{action}/{id}",
+                new { action = "Index", id = "" }
+            );
+        }
+    }
+}
